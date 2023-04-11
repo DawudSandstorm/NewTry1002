0808f3dab0114e8f05a825f0b3b9bd22f0226c6f
DawudSandstorm <98338877+DawudSandstorm@users.noreply.github.com>
2023-04-10 22:29:14 -0500

    Update movies.js

--- movies.js
+++ movies.js
@@ -15,38 +15,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 
-//Require the Router we defined in movies.js
-var movies = require('./movies.js');
+//Require the Router we defined in tickets.js
+var tickets = require('./tickets.js');
 
-//Use the Router on the sub route /movies
-app.use('/movies', movies);
+//Use the Router on the sub route /tickets
+app.use('/tickets', tickets);
 
 app.listen(3000);
 
 router.get('/', function(req, res){
-   res.json(movies);
+   res.json(tickets);
 });
 
-var movies = [
-    {id: 101, name: "Fight Club", year: 1999, rating: 8.1},
-    {id: 102, name: "Inception", year: 2010, rating: 8.7},
-    {id: 103, name: "The Dark Knight", year: 2008, rating: 9},
-    {id: 104, name: "12 Angry Men", year: 1957, rating: 8.9}
+var tickets = [
+    {id: 101, name: "Night Club", desc: "A place to party at night", type: "Building"},
+    {id: 102, name: "Inscription", desc: "An engraved writing", type: "Writing"},
+    {id: 103, name: "Dark Knight", desc: "A knight with dark coloring", type: "Person"},
+    {id: 104, name: "Angry Man", desc: "a someone displeased individual", type: "Person"}
  ];
  
  //Routes will go here
  module.exports = router;
 
  router.get('/:id([0-9]{3,})', function(req, res){
-    var currMovie = movies.filter(function(movie){
-       if(movie.id == req.params.id){
+    var currTicket = tickets.filter(function(ticket){
+       if(ticket.id == req.params.id){
           return true;
        }
     });
-    if(currMovie.length == 1){
-       res.json(currMovie[0])
+    if(currTicket.length == 1){
+       res.json(currTicket[0])
     } else {
-       res.status(404);//Set status to 404 as movie was not found
+       res.status(404);//Set status to 404 as ticket was not found
        res.json({message: "Not Found"});
     }
  });
@@ -54,58 +54,57 @@ var movies = [
  router.post('/', function(req, res){
     //Check if all fields are provided and are valid:
     if(!req.body.name ||
-       !req.body.year.toString().match(/^[0-9]{4}$/g) ||
-       !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)){
+       !req.body.desc ||
+       !req.body.type ){
        
        res.status(400);
        res.json({message: "Bad Request"});
     } else {
-       var newId = movies[movies.length-1].id+1;
-       movies.push({
+       var newId = tickets[tickets.length-1].id+1;
+       tickets.push({
           id: newId,
           name: req.body.name,
-          year: req.body.year,
-          rating: req.body.rating
+          desc: req.body.desc,
+          type: req.body.type
        });
-       res.json({message: "New movie created.", location: "/movies/" + newId});
+       res.json({message: "New ticket created.", location: "/tickets/" + newId});
     }
  });
 
  router.put('/:id', function(req, res){
    //Check if all fields are provided and are valid:
    if(!req.body.name ||
-      !req.body.year.toString().match(/^[0-9]{4}$/g) ||
-      !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+      !req.body.desc ||
+      !req.body.type ||
       !req.params.id.toString().match(/^[0-9]{3,}$/g)){
       
       res.status(400);
       res.json({message: "Bad Request"});
    } else {
-      //Gets us the index of movie with given id.
-      var updateIndex = movies.map(function(movie){
-         return movie.id;
+      //Gets us the index of ticket with given id.
+      var updateIndex = tickets.map(function(ticket){
+         return ticket.id;
       }).indexOf(parseInt(req.params.id));
       
       if(updateIndex === -1){
-         //Movie not found, create new
-         movies.push({
+         //ticket not found, create new
+         tickets.push({
             id: req.params.id,
             name: req.body.name,
-            year: req.body.year,
-            rating: req.body.rating
+            desc: req.body.desc,
+            type: req.body.type
          });
-         res.json({message: "New movie created.", location: "/movies/" + req.params.id});
+         res.json({message: "New ticket created.", location: "/tickets/" + req.params.id});
       } else {
-         //Update existing movie
-         movies[updateIndex] = {
+         //Update existing ticket
+         tickets[updateIndex] = {
             id: req.params.id,
             name: req.body.name,
-            year: req.body.year,
-            rating: req.body.rating
+            desc: req.body.desc,
+            type: req.body.type
          };
-         res.json({message: "Movie id " + req.params.id + " updated.", 
-            location: "/movies/" + req.params.id});
+         res.json({message: "Ticket id " + req.params.id + " updated.", 
+            location: "/tickets/" + req.params.id});
       }
    }
 });
-
